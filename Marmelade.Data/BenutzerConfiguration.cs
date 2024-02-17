67a64f4fede24dc71e7a4722b9d9257960137954
Marco Kittel <marco@kittel.dev>
2024-02-17 20:42:00 +0100

    Multiuser Implementation - Benutzer Entity

---
+++ Marmelade.Data/BenutzerConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Marmelade.Data
+{
+    internal class BenutzerConfiguration : IEntityTypeConfiguration<Benutzer>
+    {
+        public void Configure(EntityTypeBuilder<Benutzer> builder)
+        {
+            builder.HasData(
+                    new Benutzer
+                    {
+                        Id = 1,
+                        Name = "paul",
+                        Password = Benutzer.GetPassword("11dänemark")
+                    },
+                    new Benutzer
+                    {
+                        Id = 2,
+                        Name = "peter",
+                        Password = Benutzer.GetPassword("5812password")
+                    }
+                );
+        }
+    }
+
+}
---
+++ Marmelade.Data/Entity/Benutzer.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Marmelade.Data;
+
+public class Benutzer: BaseEntity
+{
+    public string Name { get; set;  } = String.Empty;
+    public string Password { get; set; } = String.Empty;
+    public ICollection<Lagergegenstand> Lagergegenstand { get; } = new List<Lagergegenstand>();
+    public ICollection<Lagerort> Lagerort { get; } = new List<Lagerort>();
+
+    public static string GetPassword(string password)
+    {
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            return GetHash(sha256Hash, password);
+ 
+        }
+    }
+
+    private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+    {
+        byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+        var sBuilder = new StringBuilder();
+        for (int i = 0; i < data.Length; i++)
+        {
+            sBuilder.Append(data[i].ToString("x2"));
+        }
+        return sBuilder.ToString();
+    }
+}
